5bf2d4cf35f5470fec71ba2964eb9d92d5926453
Vespertino <davidbasketyague@gmail.com>
2024-12-16 15:18:53 +0100

    Commit 16/12/24

---
+++ TFG_DavidGomez/TFG_DavidGomez/Clases/Conexion/ConexionBD.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFG_DavidGomez.Clases.Conexion
+{
+    public class ConexionBD
+    {
+        private static IMongoDatabase database;
+        private static MongoClient client;
+
+        
+        static ConexionBD()
+        {
+            string connectionString = "mongodb://localhost:27017";
+            try
+            {
+                client = new MongoClient(connectionString);
+                database = client.GetDatabase("ludotecas"); 
+                Console.WriteLine("Conexión exitosa a MongoDB.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al conectar con MongoDB: " + ex.Message);
+            }
+        }
+
+        public static IMongoDatabase GetDatabase()
+        {
+            if (database == null)
+                throw new InvalidOperationException("La base de datos no está disponible.");
+            return database;
+        }
+
+        public static IMongoCollection<T> GetCollection<T>(string nombreColeccion)
+        {
+            return database.GetCollection<T>(nombreColeccion);
+        }
+    }
+}
---
+++ TFG_DavidGomez/TFG_DavidGomez/Clases/Inscripciones.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFG_DavidGomez.Clases
+{
+    public class Inscripciones
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public ObjectId IdNino { get; set; }
+        public ObjectId IdActividad { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime FechaInscripcion { get; set; }
+
+        public string Estado { get; set; }
+
+        public Inscripciones(ObjectId idNino, ObjectId idActividad, DateTime fechaInscripcion, string estado)
+        {
+            IdNino = idNino;
+            IdActividad = idActividad;
+            FechaInscripcion = fechaInscripcion;
+            Estado = estado;
+        }
+
+        public override string ToString()
+        {
+            return $"Inscripción: Niño {IdNino}, Actividad {IdActividad}, Fecha: {FechaInscripcion}, Estado: {Estado}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Inscripciones inscripcion && Id.Equals(inscripcion.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
